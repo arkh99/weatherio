850e9a653fb237321776f6312a5a48b32d0d36e3
alireza <arkh919@gmail.com>
2024-07-08 22:18:37 -0400

    api.js and mudole.js done !

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,28 @@
-/**
- * @license MIT
- * @fileoverview All api related stuff like api_key, api request etc.
- * @copyright codewithsadee 2023 All rights reserved
- * @author codewithsadee <mohammadsadee24@gmail.com>
- */
+'use strict';
+
+const api_key = "1792672954c7ee6b677c82b3196b7dd8";
+
+
+export const fetchData = function (URL, callback) {
+    fetch(`${URL}&appid=${api_key}`)
+        .then(res => res.json())
+        .then(data => callback(data))
+}
+
+export const url = {
+    currentWeather(lat, lon) {
+        return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`
+    },
+    forecast(lat, lon) {
+        return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`
+    },
+    airPolution(lat, lon) {
+        return `http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`
+    },
+    reverseGeo(lat, lon) {
+       return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5` 
+    },
+    geo(query) {
+        return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`
+    }
+}
--- assets/js/module.js
+++ assets/js/module.js
@@ -1,6 +1,84 @@
-/**
- * @license MIT
- * @fileoverview All module functions
- * @copyright codewithsadee 2023 All rights reserved
- * @author codewithsadee <mohammadsadee24@gmail.com>
- */
+'use strict';
+
+export const weekDayNames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+
+export const monthNames = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec"
+];
+
+export const getData = function (dateUnix, timezone) {
+    const date = new Date((dateUnix + timezone) * 1000);
+    const weekDayName = weekDayNames[date.getUTCDay()];
+    const monthName = monthNames[date.getUTCMonth()];
+
+    return `${weekDayName} ${date.getUTCDate()}, ${monthName}`;
+}
+    
+export const getTime = function (timeUnix, tomezone) {
+    const date = new Date((timeUnix + timezone) * 1000);
+    const hours = date.getDateUTCHours();
+    const minutes = date.getUTCMinutes();
+    const period = hours >= 12 ? "PM" : "AM";
+
+    return `${hours % 12 || 12}:${minutes} ${period}`
+}
+
+
+export const getHours = function (timeUnix, tomezone) {
+    const date = new Date((timeUnix + timezone) * 1000);
+    const hours = date.getDateUTCHours();
+    const period = hours >= 12 ? "PM" : "AM";
+
+    return `${hours % 12 || 12} ${period}`;
+}
+
+export const mps_to_kmh = mps => {
+    const mph = mps * 3600;
+    return mph / 1000
+}
+
+export const aqiText = {
+    1: {
+        level: "Good",
+        message: "Air quality is considered satisfactory, and air pollution poses little or no risk."
+    },
+
+    2: {
+        level: fair,
+        essage: "Air quality is acceptable, however, for some pollutions there may be a moderate health for a very small number of people who are unusually sensetive to air pollution."
+    },
+
+    3: {
+        level: "Moderate",
+        message: "Members of sensitive groups may experience health effects. The general public is not likely to be affected."
+    },
+    
+    4: {
+        level: "Poor",
+        message: "Everyone may begin to experience health effects; members of sensitive groups may experiece more serious health effects."
+    },
+
+    5: {
+        level: "Very Poor",
+        message: "Health warnings of emergency conditions. The entire population is more likely to be affected."
+    }
+}
